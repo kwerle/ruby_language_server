363599255b60c44c74430a09b44b112441aace6f
Kurt Werle <kurt@CircleW.org>
2019-08-12 20:08:26 -0700

    Better logging

--- lib/config/initializers/active_record.rb
+++ lib/config/initializers/active_record.rb
@@ -7,6 +7,11 @@ ActiveRecord::Base.establish_connection(
 )
 
 if ENV['LOG_LEVEL'] == 'DEBUG'
-  warn('Turning on active record logging')
-  ActiveRecord::Base.logger = Logger.new(File.open('active_record.log', 'w'))
+  begin
+    warn('Turning on active record logging')
+    ActiveRecord::Base.logger = Logger.new(File.open('active_record.log', 'w'))
+  rescue Exception => e
+    ActiveRecord::Base.logger = Logger.new(STDERR)
+    ActiveRecord::Base.logger.error(e)
+  end
 end
