2553cb0e16f414d7675db1630e9870de510e8496
Kurt Werle <kurt@CircleW.org>
2020-02-13 20:43:21 -0800

    Skipping already installed gems may help.
    Or not.

--- lib/ruby_language_server/gem_installer.rb
+++ lib/ruby_language_server/gem_installer.rb
@@ -12,16 +12,18 @@ module RubyLanguageServer
         return if additional_gem_names.nil? || additional_gem_names.empty?
 
         RubyLanguageServer.logger.info("Trying to install gems #{additional_gem_names}")
-
+        gems_already_installed = []
         gemfile do
           source 'https://rubygems.org'
           # Lock all the gems we already have installed to the versions we have installed
           # For some reason, installing bundler makes it unhappy.  Whatever.
           Gem::Specification.reject { |s| s.name == 'bundler' }.each do |specification|
-            gem(specification.name, specification.version.to_s)
+            gem_name = specification.name
+            gem(gem_name, specification.version.to_s)
+            gems_already_installed << gem_name
           end
           additional_gem_names.each do |gem_name|
-            gem gem_name
+            gem gem_name unless gems_already_installed.include?(gem_name)
           end
         end
       end
