228af04df3542e91b994d6dca191f4e052d95aeb
Kurt Werle <kurt@CircleW.org>
2025-05-09 22:04:42 -0700

    Ditch rubocop
    Fixes #91
    
    rubocop has its own LSP. Stop wrestling with it. It was never worth the effort anyway.

--- lib/ruby_language_server/server.rb
+++ lib/ruby_language_server/server.rb
@@ -5,6 +5,7 @@ require 'json'
 # Deal with the various languageserver calls.
 module RubyLanguageServer
   class Server
+    Position = Struct.new('Position', :line, :character)
     attr_accessor :io
 
     def initialize(mutex)
@@ -18,9 +19,6 @@ module RubyLanguageServer
       root_uri = params['rootUri']
       @project_manager = ProjectManager.new(root_path, root_uri)
       @project_manager.scan_all_project_files
-      gem_string = ENV.fetch('ADDITIONAL_GEMS', nil)
-      gem_array = (gem_string.split(',').compact.map(&:strip).reject { |string| string == '' } if gem_string && !gem_string.empty?)
-      @project_manager.install_additional_gems(gem_array)
       {
         capabilities: {
           textDocumentSync: 1,
@@ -124,11 +122,9 @@ module RubyLanguageServer
       textDocument['uri']
     end
 
-    Position = Struct.new('Position', :line, :character)
-
     def postition_from_params(params)
       position = params['position']
-      Position.new((position['line']).to_i, position['character'].to_i)
+      Position.new(position['line'].to_i, position['character'].to_i)
     end
   end
 end
