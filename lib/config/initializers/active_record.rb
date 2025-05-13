6f2d487688c47ed1d519cc762232977bcf8cf9c7
Kurt Werle <kurt@CircleW.org>
2025-05-12 20:36:44 -0700

    Try loading the extension every time AR loads

--- lib/config/initializers/active_record.rb
+++ lib/config/initializers/active_record.rb
@@ -7,15 +7,17 @@ ActiveRecord::Base.establish_connection(
   timeout: 30.seconds # does not seem to help
 )
 
-database = ActiveRecord::Base.connection.raw_connection
-database.enable_load_extension(1)
-if Gem.win_platform?
-  # load DLL from PATH
-  database.load_extension('liblevenshtein.dll')
-else
-  database.load_extension('/usr/local/lib/liblevenshtein.so.0.0.0')
+ActiveSupport.on_load(:active_record) do
+  database = ActiveRecord::Base.connection.raw_connection
+  database.enable_load_extension(1)
+  if Gem.win_platform?
+    # load DLL from PATH
+    database.load_extension('liblevenshtein.dll')
+  else
+    database.load_extension('/usr/local/lib/liblevenshtein.so.0.0.0')
+  end
+  database.enable_load_extension(0)
 end
-database.enable_load_extension(0)
 
 if ENV['LOG_LEVEL'] == 'DEBUG'
   begin
