857084663c0dcc7fbf7473dd773e5f086d715ba2
Kurt Werle <kurt@CircleW.org>
2025-08-03 11:23:12 -0700

    Add support for using sockets using LSP_PORT to set it.

--- lib/ruby_language_server/io.rb
+++ lib/ruby_language_server/io.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'socket'
 
 module RubyLanguageServer
   class IO
+    attr_reader :using_socket
+
     def initialize(server, mutex)
       @server = server
       @mutex = mutex
       server.io = self
+
+      configure_io
+
       loop do
-        (id, response) = process_request($stdin)
-        return_response(id, response, $stdout) unless id.nil?
+        (id, response) = process_request(@in)
+        return_response(id, response, @out) unless id.nil?
       rescue SignalException => e
         RubyLanguageServer.logger.error "We received a signal.  Let's bail: #{e}"
         exit
@@ -18,10 +24,36 @@ module RubyLanguageServer
         RubyLanguageServer.logger.error "Something when horribly wrong: #{e}"
         backtrace = e.backtrace * "\n"
         RubyLanguageServer.logger.error "Backtrace:\n#{backtrace}"
+      if @using_socket && @in
+        begin
+          @in.close
+        rescue
+        end
       end
     end
 
-    def return_response(id, response, io = $stdout)
+    private
+
+    attr_accessor :in, :out
+
+    def configure_io
+      if ENV['LSP_PORT']
+        @tcp_server = TCPServer.new(ENV['LSP_PORT'].to_i)
+        RubyLanguageServer.logger.info "Listening on TCP port #{ENV['LSP_PORT']} for LSP connections"
+        self.in = @tcp_server.accept
+        self.out = self.in
+        @using_socket = true
+        RubyLanguageServer.logger.info "Accepted LSP socket connection"
+      else
+        self.in = $stdin
+        self.out = $stdout
+        @using_socket = false
+      end
+    end
+    end
+
+    def return_response(id, response, io = nil)
+      io ||= out
       full_response = {
         jsonrpc: '2.0',
         id:,
@@ -29,13 +61,14 @@ module RubyLanguageServer
       }
       response_body = JSON.unparse(full_response)
       RubyLanguageServer.logger.info "return_response body: #{response_body}"
-      io.write "Content-Length: #{response_body.length + 0}\r\n"
+      io.write "Content-Length: #{response_body.length}\r\n"
       io.write "\r\n"
       io.write response_body
-      io.flush
+      io.flush if io.respond_to?(:flush)
     end
 
-    def send_notification(message, params, io = $stdout)
+    def send_notification(message, params, io = nil)
+      io ||= out
       full_response = {
         jsonrpc: '2.0',
         method: message,
@@ -43,13 +76,14 @@ module RubyLanguageServer
       }
       body = JSON.unparse(full_response)
       RubyLanguageServer.logger.info "send_notification body: #{body}"
-      io.write "Content-Length: #{body.length + 0}\r\n"
+      io.write "Content-Length: #{body.length}\r\n"
       io.write "\r\n"
       io.write body
-      io.flush
+      io.flush if io.respond_to?(:flush)
     end
 
-    def process_request(io = $stdin)
+    def process_request(io = nil)
+      io ||= self.in
       request_body = get_request(io)
       # RubyLanguageServer.logger.debug "request_body: #{request_body}"
       request_json = JSON.parse request_body
@@ -77,7 +111,8 @@ module RubyLanguageServer
       end
     end
 
-    def get_request(io = $stdin)
+    def get_request(io = nil)
+      io ||= self.in
       initial_line = get_initial_request_line(io)
       RubyLanguageServer.logger.debug "initial_line: #{initial_line}"
       length = get_length(initial_line)
@@ -95,47 +130,19 @@ module RubyLanguageServer
       content
     end
 
-    def get_initial_request_line(io = $stdin)
+    def get_initial_request_line(io = nil)
+      io ||= self.in
       io.gets
     end
 
     def get_length(string)
       return 0 if string.nil?
-
       string.match(/Content-Length: (\d+)/)[1].to_i
     end
 
-    def get_content(size, io = $stdin)
+    def get_content(size, io = nil)
+      io ||= self.in
       io.read(size)
     end
-
-    # http://www.alecjacobson.com/weblog/?p=75
-    # def stdin_read_char
-    #   begin
-    #     # save previous state of stty
-    #     old_state = `stty -g`
-    #     # disable echoing and enable raw (not having to press enter)
-    #     system "stty raw -echo"
-    #     c = STDIN.getc.chr
-    #     # gather next two characters of special keys
-    #     if(c=="\e")
-    #       extra_thread = Thread.new{
-    #         c = c + STDIN.getc.chr
-    #         c = c + STDIN.getc.chr
-    #       }
-    #       # wait just long enough for special keys to get swallowed
-    #       extra_thread.join(0.00001)
-    #       # kill thread so not-so-long special keys don't wait on getc
-    #       extra_thread.kill
-    #     end
-    #   rescue Exception => ex
-    #     puts "#{ex.class}: #{ex.message}"
-    #     puts ex.backtrace
-    #   ensure
-    #     # restore previous state of stty
-    #     system "stty #{old_state}"
-    #   end
-    #   return c
-    # end
   end # class
 end # module
